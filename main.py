2e9fc8822bc0ece68f760018ef4728e8f9e54837
Vann4 <1vetlan1@mail.ru>
2023-09-14 19:49:42 +0300

    Изменение решения, убрал цикл и изменил текст в print

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 #Задача, пользователь вводит строку состоящую из Р(решка) и О(орёл), подсчитать сколько Р введено подряд, если ни одной то вывести 0
 user_string = input('Введите Р и О: ').lower().split('о')
 user_string_count = 0
-print()
-for i in max(user_string):
-    user_string_count += i.count('р')
-print('Столько раз вы ввели Р(решку):',user_string_count)
+user_string_count += max(user_string).count('р')
+print('\nМаксимальное количество подряд выпавших Р(решек):',user_string_count)

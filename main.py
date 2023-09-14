add44e5b8a0cfdda7f1c0422b7c918e3e4bf66b7
Vann4 <1vetlan1@mail.ru>
2023-09-14 19:19:37 +0300

    Решение задачи

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #Задача, пользователь вводит строку состоящую из Р(решка) и О(орёл), подсчитать сколько Р введено подряд, если ни одной то вывести 0
+user_string = input('Введите Р и О: ').lower().split('о')
+user_string_count = 0
+print()
+for i in max(user_string):
+    user_string_count += i.count('р')
+print('Столько раз вы ввели Р(решку):',user_string_count)
